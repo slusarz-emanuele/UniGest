a6450655fb8f6944e1a22d3f328c2a9ccc750889
Francesco Patricelli <francesco.patricelli@student.univaq.it>
2025-08-28 20:04:11 +0200

    correzione costruttore,aggiunta commenti,indentazione,import e metodo getEta()

--- model/Persona.java
+++ model/Persona.java
@@ -1,9 +1,12 @@
 package it.univaq.unigest.model;
+ 
+ import it.univaq.unigest.util.LocalDateUtil;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 public abstract class Persona{
+   //attributi
     private String cf;
     private String nome;
     private String cognome;
@@ -11,56 +14,80 @@ public abstract class Persona{
     private String dataNascita;
     private String dataIngressoUniversita;
 
-    protected Persona(private String cf,
-    private String nome,
-    private String cognome,
-    private LocalDate dataNascita,
-    private String dataIngressoUniversita){
-                   this.cf=cf;
-                   this.nome=nome;
-                   this.cognome=cognome;
-                   this.email= generaEmail();
-                   this.dataNascita= LocalDateUtil.toString(dataNascita);
-                   this.dataIngressoUniversita=dataIngressoUniversita;
+    //costruttore
+    protected Persona( String cf,
+                       String nome,
+                       String cognome,
+                       LocalDate dataNascita,
+                       String dataIngressoUniversita){
+                   this.cf = cf;
+                   this.nome = nome;
+                   this.cognome = cognome;
+                   this.email = generaEmail();
+                   this.dataNascita = LocalDateUtil.toString(dataNascita);
+                   this.dataIngressoUniversita = dataIngressoUniversita;
      }
+
+     //metodi getter
      public String getCf(){
         return this.cf;
      }
+
      public String getNome(){
         return this.nome;
      }
+
      public String getEmail(){
         return this.email;
      }
+
      public String getDataNascita(){
         return this.dataNascita;
      }
+
      public String getDataIngressoUniversita(){
         return this.dataIngressoUniversita;
      }
+
      public String getEta(){
-        //da fare
+      LocalDate nascita = LocalDateUtil.fromString(this.dataNascita);
+      if(nascita == null) return "-";
+
+      LocalDate oggi = LocalDate.now();
+      Period eta = Period.between(nascita,oggi);
+
+      return eta.getYears() + " anni";
      }
 
+    //metodi setter
     public void setCf(String cf){
-        this.cf=cf;
+        this.cf = cf;
     }
+
         public void setNome(String nome){
-        this.nome=nome;
+        this.nome = nome;
     }
+
         public void setCognome(String cognome){
-        this.cognome=cognome;
+        this.cognome = cognome;
     }
+
         public void setEmail(String email){
-        this.email=email;
+        this.email = email;
     }
+
         public void setDataNascita(String DataNascita){
-        this.dataNascita=DataNascita;
+        this.dataNascita = DataNascita;
     }
+
         public void setDataIngressoUniversita(String DataIngressoUniversita){
-        this.dataIngressoUniversita=DataIngressoUniversita;
+        this.dataIngressoUniversita = DataIngressoUniversita;
     }
+
+    //metodo astratto
     protected abstract String generaEmail();
+
+    //metodo toString
     public String toString(){
         return "cf(id): " + this.cf + ", " +
                 "Nome: " + this.nome + ", " +

a8dc18f04bb1c352a2cc77950836c1620f9dbb00
Francesco Patricelli <francesco.patricelli@student.univaq.it>
2025-08-28 21:09:53 +0200

    applicate modifiche di :indentazione , commenti, import di classi manager e implementazione del metodo per caricare tutti gli esami dinamicamente

--- src/main/java/it/univaq/unigest/model/Verbale.java
+++ src/main/java/it/univaq/unigest/model/Verbale.java
@@ -1,9 +1,15 @@
 package it.univaq.unigest.model;
 
+import it.univaq.unigest.manager.AppelloManager;
+import it.univaq.unigest.manager.EsameManager;
+import it.univaq.unigest.manager.IscrizioneManager;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public class Verbale{
+    
+    //attributi
     private Integer id;
     private String appelloId;
     private LocalDate dataChiusura;
@@ -19,14 +25,16 @@ boolean chiuso,
 boolean firmato,
 String note,
 List<Esame> esami){
-    this.id=id;
-    this.appelloId=appelloId;
-    this.dataChiusura=dataChiusura;
-    this.chiuso=chiuso;
-    this.firmato=firmato;
-    this.note=note;
-    this.esami=esami;
+    this.id = id;
+    this.appelloId = appelloId;
+    this.dataChiusura = dataChiusura;
+    this.chiuso = chiuso;
+    this.firmato = firmato;
+    this.note = note;
+    this.esami = esami;
 }
+
+    //metodi getter
 public Integer getId() { return id; }
 
     public String getAppelloId() { return appelloId; }
@@ -41,7 +49,7 @@ public Integer getId() { return id; }
 
     public List<Esame> getEsami() { return esami; }
 
-
+    //metodi setter
     public void setId(Integer id) { this.id = id; }
 
     public void setAppelloId(String appelloId) { this.appelloId = appelloId; }
@@ -54,8 +62,29 @@ public Integer getId() { return id; }
 
     public void setNote(String note) { this.note = note; }
 
-    public void setEsami(List<Esame> esami) { this.esami = esami; }
-    
+    public void setEsami(List<Esame> esami) { this.esami = esami; } 
+
+     //metodo per caricare tutti gli esami dinamicamente
+    public void caricaEsamiDinamicamente(IscrizioneManager iscrizioneManager,
+                                        EsameManager esameManager){
+        if(this.getId() == null) return ;
+
+        //1) dagli appelli troviamo le iscrizioni 
+        List<Integer> iscrizioniIds = iscrizioneManager.getAll().stream()
+            .filter(i -> i.getRidAppello() == Integer.parseInt(this.getAppelloId()))
+            .map(Iscrizione::getId())
+            .toList();
+
+        //2) dalle iscrizioni troviamo gli esami
+        List<Esame> esamiTrovvati = esameManager.getAll().stream()
+            .filter(e -> iscrizioniIds.contains(Integer.valueOf(e.getIscrizioneId())))
+            .toList();
+
+        //3) salviamo tutto 
+        this.setEsami(esamiTrovvati);
+        }
+
+    //override toString
      public String toString() {
         return "id: " + id +
                 ", appelloId: " + appelloId +

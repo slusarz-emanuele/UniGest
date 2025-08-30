3898e87c78ef31c9922a1a7edf908d54e89d5b75
slusarz-emanuele <emanuele.slusarz@gmail.com>
2025-08-31 00:28:51 +0200

    Aggiunta del pannello Docenti

---
+++ src/main/java/it/univaq/unigest/gui/modelview/pannelli/docenti/DocentiPannello2.java
@@ -0,0 +1,25 @@
+package it.univaq.unigest.gui.modelview.pannelli.docenti;
+
+import javafx.scene.layout.VBox;
+
+public class DocentiPannello2 {
+    
+    public static VBox getView(){
+        return null;
+        /**
+         * TODO: completare implementazione del metodo con tabella e gesione delle funzionalità
+         */
+    }
+
+    public static void apriDialogAggiungi(){
+
+    }
+
+    public static void modificaSelezionato(){
+
+    }
+
+    public static void eliminaSelezionato(){
+
+    }
+}

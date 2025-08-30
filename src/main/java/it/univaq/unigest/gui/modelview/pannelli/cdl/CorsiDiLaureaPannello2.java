e69283930962cde734c9a8ac4926a4caa792b1eb
Francesco Patricelli <francesco.patricelli@student.univaq.it>
2025-08-30 23:51:57 +0200

    Test

---
+++ src/main/java/it/univaq/unigest/gui/modelview/pannelli/cdl/CorsiDiLaureaPannello2.java
@@ -0,0 +1,13 @@
+package it.univaq.unigest.gui.modelview.pannelli.cdl;
+
+import javafx.scene.layout.VBox;
+
+public class CorsiDiLaureaPannello2 {
+    
+    public static VBox getView(){
+        return null;
+        /** 
+         * TODO: completare implementazione del metodo con riepilogo delle statistiche e grafico
+         */
+    }
+}

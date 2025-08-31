c2c747180468a8c8acf46f9bc16a8104834eb8a9
Francesco Patricelli <francesco.patricelli@student.univaq.it>
2025-08-31 03:17:41 +0200

    Adattatore personalizzato per la gestione della serializzazione e deserializzazione della LocalTime con Gson

---
+++ src/main/java/it/univaq/unigest/gui/componenti/LocalTimeAdapter.java
@@ -0,0 +1,37 @@
+package it.univaq.unigest.gui.componenti;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+/**
+ * Adattatore personalizzato per la gestione della serializzazione e deserializzazione
+ * di {@link LocalTime} con la libreria Gson
+ */
+public class LocalTimeAdapter extends TypeAdapter<LocalTime> {
+
+    /**
+     * Scrive il valore su JSON della data.
+     * @param out Il writer JSON di tipo {@link JsonWriter}.
+     * @param value Il valore data di tipo {@link LocalTime}.
+     * @throws IOException Se il formato della data è errato o il writer è errato.
+     */
+    @Override
+    public void write(JsonWriter out, LocalTime value) throws IOException {
+        out.value(value.toString());
+    }
+
+    /**
+     * Legge e restituisce il valore da JSON della data.
+     * @param in Il reader JSON di tipo {@link JsonReader}.
+     * @return Oggetto di tipo {@link LocalTime} letto da JSON.
+     * @throws IOException Se il formato della data è errato o il reader è errato.
+     */
+    @Override
+    public LocalTime read(JsonReader in) throws IOException {
+        return LocalTime.parse(in.nextString());
+    }
+}
